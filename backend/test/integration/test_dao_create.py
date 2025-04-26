377615389d7fdc6d49594f9073172889a201f4f7
albinr <albin@rybergs.net>
2025-04-26 12:54:12 +0200

    Added more integration tests

--- backend/test/integration/test_dao_create.py
+++ backend/test/integration/test_dao_create.py
@@ -45,13 +45,39 @@ def test_duplicate_description_raises_writeerror(dao):
   Test that inserting a duplicate 'description' raises WriteError due to uniqueness constraint.
   """
   # Arrange
-  test_input_data = {"description": "Duplicate task", "done": False}
+  test_input_data = {"description": "Duplicate task", "done": False} # First insert
   dao.create(test_input_data)
 
   # Assert
   with pytest.raises(WriteError):
     # Act
-    dao.create(test_input_data)
+    dao.create(test_input_data) # Second insert with same description
+
+
+@pytest.mark.integration
+@pytest.mark.lab1
+@pytest.mark.lab1assignment3
+def test_int_input_raises_writeerror(dao):
+  """
+  Test case 3
+  Test that inserting a int raises writeerror.
+  """
+  test_input_data = 2
+  with pytest.raises(WriteError):
+      dao.create(test_input_data)
+
+
+@pytest.mark.integration
+@pytest.mark.lab1
+@pytest.mark.lab1assignment3
+def test_list_input_raises_writeerror(dao):
+  """
+  Test case 4
+  Test that inserting a int raises writeerror.
+  """  
+  test_input_data = ["description", "List input", "done", True]
+  with pytest.raises(WriteError):
+      dao.create(test_input_data)
 
 
 @pytest.mark.integration
@@ -59,7 +85,7 @@ def test_duplicate_description_raises_writeerror(dao):
 @pytest.mark.lab1assignment3
 def test_missing_properties_raises_writeerror(dao):
   """
-  Test case 3
+  Test case 5
   Test that invalid input data raises WriteError (missing required properties).
   """
   # Arrange
@@ -77,8 +103,8 @@ def test_missing_properties_raises_writeerror(dao):
 @pytest.mark.lab1assignment3
 def test_valid_todo_without_done_is_inserted(dao):
   """
-  Test case 4
-  Test that a todo with only 'description' is inserted (since 'done' is optional).
+  Test case 6
+  Test that a todo with only 'description' is inserted (since "done" is optional).
   """
   # Arrange
   test_input_data = {
@@ -98,8 +124,8 @@ def test_valid_todo_without_done_is_inserted(dao):
 @pytest.mark.lab1assignment3
 def test_invalid_data_raises_writeerror(dao):
   """
-  Test case 5
-  Test that missing required 'description' raises WriteError.
+  Test case 7
+  Test that missing required "description" raises WriteError.
   """
   # Arrange
   test_input_data = {"blabla": 100, "wrong": "true"}
@@ -113,36 +139,36 @@ def test_invalid_data_raises_writeerror(dao):
 @pytest.mark.lab1
 @pytest.mark.lab1assignment3
 def test_input_empty_dict_raises_writeerror(dao):
-    """
-    Test case 6
-    Test that invalid 'done' type raises WriteError.
-    """
-    test_input_data = {}
-    with pytest.raises(WriteError):
-        dao.create(test_input_data)
+  """
+  Test case 8
+  Test that inserts empty dict type raises WriteError.
+  """
+  test_input_data = {}
+  with pytest.raises(WriteError):
+      dao.create(test_input_data)
 
 
 @pytest.mark.integration
 @pytest.mark.lab1
 @pytest.mark.lab1assignment3
 def test_invalid_done_type_raises_writeerror(dao):
-    """
-    Test case 7
-    Test that invalid 'done' type raises WriteError.
-    """
-    test_input_data = {"description": "wrong type", "done": "no"}
-    with pytest.raises(WriteError):
-        dao.create(test_input_data)
+  """
+  Test case 9
+  Test that invalid "done" type raises WriteError.
+  """
+  test_input_data = {"description": "wrong type", "done": "no"}
+  with pytest.raises(WriteError):
+      dao.create(test_input_data)
 
 
 @pytest.mark.integration
 @pytest.mark.lab1
 @pytest.mark.lab1assignment3
 def test_extra_field_is_allowed(dao):
-    """
-    Test case 8
-    Test that extra field 'extra' is allowed and stored correctly.
-    """
-    test_input_data = {"description": "test with extra field", "done": True, "extra": "extra field"}
-    result = dao.create(test_input_data)
-    assert result["extra"] == "extra field"
+  """
+  Test case 10
+  Test that extra field "extra" is allowed and stored correctly.
+  """
+  test_input_data = {"description": "test with extra field", "done": True, "extra": "extra field"}
+  result = dao.create(test_input_data)
+  assert result["extra"] == "extra field"
